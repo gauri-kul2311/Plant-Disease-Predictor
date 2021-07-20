33723540df7abcc1a5039051271c55b2ca88b6f5
Gauri Kulkarni <69246149+gauri-kul2311@users.noreply.github.com>
2021-07-20 21:57:22 +0530

    Add files via upload

--- src/components/home/HomeHead.vue
+++ src/components/home/HomeHead.vue
@@ -1,1 +1,60 @@
+<template>
+  <div class="head">
+    <div class="float-left">
+      <table class="above-base">
+        <tr>
+          <td class="pc">
+            <p class="op">
+              <img width="50px" height="50px" class="plant-cartoon" src="hang.jpg" />
+            </p>
+          </td>
+          <td class="sp">
+            <div class="op">
+              <h5 class="text-center text-bold">PLANT DISEASE PREDICTOR</h5>
+              <h6 class="large-screen-only">
+                Upload the picture of diseased plant and get the disease predicted! 
+              </h6>
+              <q-btn class="get-started"
+                to="/getstarted"
+                size="1em">
+                <q-icon left size="2em" name="eco" />
+                <div>Get Started</div>
+              </q-btn>
+            </div>
+          </td>
+          <td class="pc">
+            <p class="op large-screen-only">
+              <img class="plant-cartoon "  src="hang.jpg" />
+            </p>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
 
+<script>
+export default {};
+</script>
+
+<style>
+.btn{
+  height: 40px;
+  background-color: rgb(71, 107, 50);
+  margin-left: 10px;
+  margin-right: 10px;
+  padding-bottom: 10px;
+  text-align: center;
+}
+.btn-home{
+    height: 30px;
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  text-align: center;
+}
+.get-started{
+    background-color: rgb(71, 107, 50);
+    color: white;
+}
+</style>
---
+++ src/components/home/HomeMid.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-page class="mid">
+    <div class="fit row inline wrap justify-around items-center content-center">
+      <div class="col-12 col-sm-3 p1">
+        <div class="op">
+          <img class="option" src="1.png" />
+          <q-btn
+            class="btn1 q-mb-lg"
+            flat
+            to="/symptoms"
+            label="Know More about Plant Diseases"
+            size="15px"
+            icon-right="eva-diagonal-arrow-right-up-outline"
+            dense
+          />
+        </div>
+      </div>
+
+      <div class="col-12 col-sm-3 p1">
+        <div class="op">
+          <img class="option" src="3.jpg" />
+          <q-btn
+            class="btn1 q-mb-lg"
+            flat
+            to="/planthealthy"
+            label="How to keep your plants healthy"
+            size="15px"
+            icon-right="eva-diagonal-arrow-right-up-outline"
+            dense
+          />
+        </div>
+      </div>
+
+      <div class="col-12 col-sm-3 p1">
+        <div class="op">
+          <img class="option" src="4.jpg" />
+          <q-btn
+            class="btn1 q-mb-lg"
+            flat
+            to="/solution"
+            label="Get Solutions for plant diseases"
+            size="15px"
+            icon-right="eva-diagonal-arrow-right-up-outline"
+            dense
+          />
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.p1 {
+  border-radius: 50px;
+}
+</style>
