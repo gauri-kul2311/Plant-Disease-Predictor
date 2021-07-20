01548d3ea47ad9c00c16223b7d7feb1e1db5c744
Gauri Kulkarni <69246149+gauri-kul2311@users.noreply.github.com>
2021-07-20 21:54:58 +0530

    Add files via upload

---
+++ src/components/PlantCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-card
+    class="q-pa-md cardBack text-black text-bold fontChange"
+    style="border-radius:20px; width:150px;height:150px;"
+  >
+    <div class="text-center column">
+      <span class="flex flex-center">
+        <q-img :src="plant.image" width="35px" height="35px" />
+      </span>
+      <br />
+      {{ plant.name }}
+    </div>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: ["plant"]
+};
+</script>
+
+<style></style>
