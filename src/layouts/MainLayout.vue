3958273b465931fe703977376b139cc9bf510ef8
Gauri Kulkarni <69246149+gauri-kul2311@users.noreply.github.com>
2021-07-20 22:04:01 +0530

    Add files via upload

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,1 +1,41 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="tool">
+        <q-toolbar-title class="q-pl-lg">
+          Disease Predictor
+        </q-toolbar-title>
+        <div class="absolute-right">
+          <q-btn class="btn-home" to="/" flat icon="home" size="15px" />
+          <!-- <q-btn
+            class="btn"
+            to="/getstarted"
+            flat
+            label="Get Started"
+            size="15px"
+          /> -->
+          <!-- <q-btn class="btn" flat label="Diseases" size="15px" />
+          <q-btn class="btn" flat label="History" size="15px" /> -->
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
 
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+.tool {
+  background-color: rgb(39, 61, 37);
+}
+
+</style>
